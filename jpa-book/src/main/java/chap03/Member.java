8c2b82ea0c11abf9d420bf4b0f4f820838680e82
soheeoott <soheeoott@gmail.com>
2021-08-30 04:41:45 +0900

    Add : 데이터가 존재하는 필드만 저장하는 동적 저장 쿼리

--- jpa-book/src/main/java/chap03/Member.java
+++ jpa-book/src/main/java/chap03/Member.java
@@ -2,6 +2,7 @@ package chap03;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,6 +12,7 @@ import javax.persistence.Table;
 @Setter @Getter
 @Entity
 @org.hibernate.annotations.DynamicUpdate // 동적 수정 쿼리 : 수정된 데이터만 사용한 쿼리(update) 생성
+@DynamicInsert // 동적 저장 쿼리 : 데이터가 존재하는 필드만으로 쿼리(insert) 생성
 @Table(name = "MEMBER")
 public class Member {
 

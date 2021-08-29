c038716156e8c63f75e038de196fecdc296c7fab
soheeoott <soheeoott@gmail.com>
2021-08-30 03:26:52 +0900

    Add : 회원 엔티티

---
+++ jpa-book/src/main/java/chap02/Member.java
@@ -0,0 +1,23 @@
+package chap02;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Setter @Getter
+@Entity
+@Table(name = "MEMBER")
+public class Member {
+
+    @Id
+    @Column(name = "ID")
+    private String id;
+
+    @Column(name = "NAME")
+    private String username;
+    private Integer age;
+}
